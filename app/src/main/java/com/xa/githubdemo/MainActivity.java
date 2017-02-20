33adbccf5b37edf0c188583ca0f3b5c1c005a4c2
xaguo <xa_guo@163.com>
2017-02-21 00:00:42 +0800

    添加了Button

--- app/src/main/java/com/xa/githubdemo/MainActivity.java
+++ app/src/main/java/com/xa/githubdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.xa.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "helloworld!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
